e977274ac5e0fde0350c3dd848e31a820a075926
hikk9564 <hyoeunzz09@gmail.com>
2026-08-28 20:38:47 +0900

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,3 +1,4 @@
+import { getStorage } from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
@@ -25,6 +26,8 @@ export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
 
 export const db = getFirestore(app);
+
+export const storage = getStorage(app);
 
 
 
