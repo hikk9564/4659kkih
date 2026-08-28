f79dd69c1276d1117f8fb917566513f80982a72e
hikk9564 <hyoeunzz09@gmail.com>
2026-08-28 19:11:19 +0900

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,16 +1,24 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey:"AIzaSyCDsgfBfx301SoUxtykMRA1fqNLP44qp48",
+  apiKey: "AIzaSyCDsgfBfx301SoUxtykMRA1fqNLP44qp48",
   authDomain: "ohome-9be4d.firebaseapp.com",
   databaseURL: "ohome-9be4d.firebaseapp.com",
-  projectId: "ohome-9be4d.firebasestorage.app",
+  projectId: "ohome-9be4d",
   storageBucket: "ohome-9be4d.firebasestorage.app",
-  messagingSenderId: "574380709966",
+  messagingSenderId:"574380709966",
   appId: "1:574380709966:web:36f683c3ccf9c42f7ecb1f",
 };
 
 const app = initializeApp(firebaseConfig);
 
+// 기존 Realtime Database
 export const database = getDatabase(app);
+
+// Google 로그인
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+
+
